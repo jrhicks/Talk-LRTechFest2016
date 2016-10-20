f28793d6f2d4c303b877561c56edc265d73099ad
Jeffrey Hicks <jrhicks@gmail.com>
2016-10-20 01:25:08 -0500

    close ruby connection

--- PHPService/index.php
+++ PHPService/index.php
@@ -1,4 +1,5 @@
 <?php
 header('Content-Type: text/HTML');
+header('Connection: close');
 echo 'Hello LRTechFest!  PHP says hello.'
 ?>
--- RubyService/app.rb
+++ RubyService/app.rb
@@ -2,5 +2,6 @@ require 'sinatra'
 set :bind, '0.0.0.0'
 
 get '/' do
+  response.headers['Connection']='close'
   'Hey LRTechFest.  Ruby says Hi.'
 end
