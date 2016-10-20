b87e2fa6054ff31fe7cde6681e68800c51889e98
Jeffrey Hicks <jrhicks@gmail.com>
2016-10-20 01:04:27 -0500

    disable persistent connections for node

--- NodeService/app.js
+++ NodeService/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.writeHead(200, { 'Content-Type': 'text/HTML', 'Connection': 'close' });
     res.write("Hey LRTechFest.  Node says Hi.");
     res.end();
 });
